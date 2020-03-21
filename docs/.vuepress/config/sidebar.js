dc1784dd4573847a84f4fa4807ec64fc1e108112
Benjamin Ren <br.github@outlook.com>
2020-03-21 15:15:05 +0800

    :pencil2: 网易云课堂 高级前端开发工程师 课程内容

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -4,13 +4,13 @@ module.exports = {
     title: '前端',
     children: [{
       title: '网易云课堂 高级开发工程师',
-      path: '/note/163-study-senior-fe/',
+      path: '/note/netease-study-senior-fe/',
       children: [{
         title: '前端基础预习课',
         children: [{
           title: '页面架构',
           children: [
-            ['163-study-senior-fe/preparation/i18n', '国际化'],
+            ['netease-study-senior-fe/preparation/i18n', '国际化'],
           ],
         }],
       }],
